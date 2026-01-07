e5436489483f0e7a1d7d2ebba5079daaedec1a30
HoaPolarium <hoa.x.vu@polarium.com>
2026-01-07 10:51:47 +0700

    Thay doi API key

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
 
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
-SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+SUPABASE_KEY = os.environ.get("SUPABASE_SERVICE_ROLE_KEY")
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise RuntimeError("Please set SUPABASE_URL and SUPABASE_KEY environment variables")
 
